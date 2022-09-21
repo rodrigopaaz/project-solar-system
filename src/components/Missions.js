87c9a6cf305d0e60027c0f229ab215e38e03eea0
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-09-21 16:53:24 -0300

    06 realizado o requisito 10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map((mission, i) => (<MissionCard
+          key={ i }
+          name={ mission.name }
+          year={ mission.year }
+          country={ mission.country }
+          destination={ mission.destination }
+        />))}
       </div>
     );
   }
